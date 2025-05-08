e81267fc449d5a6275e4ae9881f4241e735624a9
NeleSuffo <nele.suffo@leuphana.de>
2025-05-08 14:40:56 +0200

    Update versioning to use versioneer for dynamic version retrieval

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         ],
     },
 
-    version="0.0.1+jsalt.v5.344",
+    version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 
     description='Neural building blocks for speaker diarization',
